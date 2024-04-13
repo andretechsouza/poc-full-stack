9d19ebc57c97c3b5b0e18eb9cd25693b97702933
André Luiz <andretechsouza@outlook.com>
2024-04-13 09:32:16 -0300

    Commit initial

---
+++ calculator-api/src/main/java/com/andretech/calculatorapi/configs/SpringDocConfig.java
@@ -0,0 +1,18 @@
+package com.andretech.calculatorapi.configs;
+
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringDocConfig {
+
+    @Bean
+    public GroupedOpenApi api() {
+        return GroupedOpenApi.builder()
+                .group("api")
+                .pathsToMatch("/**")
+                .build();
+    }
+}
+
